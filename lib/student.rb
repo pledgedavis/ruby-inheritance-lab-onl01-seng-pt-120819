6e2b28c2eccbb7b28ecb62f11a224307d7e8cc91
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 23:24:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
-class Student
+class Student < User
 attr_accessor :first_name, :last_name
 # def initialize(first_name, last_name)
 # @first_name = first_name
 # @last_name = last_name
-#
+# 
 # end
 
 
