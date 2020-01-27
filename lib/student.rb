c3f9ed198c5b4bc77b6fe7d3c43b42e9830c8531
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 23:45:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+# attr_accessor :first_name, :last_name
+ def initialize
+ @knowledge = []
+
+end
+
+def learn(string)
+@knowledge.push(string)
+end
+# @first_name = first_name
+# @last_name = last_name
+#
+# end
+def knowledge
+  @knowledge
+end
 
 end
